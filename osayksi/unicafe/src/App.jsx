c6cd936dfd0f38687e9b55857f6261d975efa674
Niko Nissinen <Niko.Nissinenn@outlook.com>
2025-09-15 09:40:03 +0300

    Tehtävä 1.7

--- osayksi/unicafe/src/App.jsx
+++ osayksi/unicafe/src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  
+  const all = good + neutral + bad;
+  const average = all ? ((good - bad) / all).toFixed(4): 0;
+  const positive = all ? ((good / all) * 100).toFixed(4) + " %" : 0;
+ 
 
   return (
     <div>
@@ -17,6 +22,9 @@ const App = () => {
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {all}</p>
+      <p>Average {average}</p>
+      <p>Positive {positive}</p>
     </div>
   )
 }
