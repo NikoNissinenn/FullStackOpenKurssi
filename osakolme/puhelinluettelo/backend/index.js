bb165ff94e3c6ee137f5178d0c27f38f5896f142
Niko Nissinen <Niko.Nissinenn@outlook.com>
2025-09-22 12:45:28 +0300

    Tehtävä 3.3

--- osakolme/puhelinluettelo/backend/index.js
+++ osakolme/puhelinluettelo/backend/index.js
@@ -43,5 +43,16 @@ app.get("/info", (request, response) => {
     `)
 })
 
+app.get(`/api/persons/:id`, (request, response) => {
+  const id = request.params.id
+  const person = persons.find((person) => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 app.listen(PORT)
 console.log(`Server running on http://localhost:${PORT}`)
