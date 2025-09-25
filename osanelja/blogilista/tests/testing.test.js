f0b85434b0596da31f8b5fd1d0ff0e873be889f3
Niko Nissinen <Niko.Nissinenn@outlook.com>
2025-09-25 09:56:15 +0300

    Tehtävä 4.4

--- osanelja/blogilista/tests/testing.test.js
+++ osanelja/blogilista/tests/testing.test.js
@@ -8,3 +8,18 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe('Total likes', () => {
+  test('Of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+  test('When list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listHelper.onlyOneBlog)
+    assert.strictEqual(result, 7)
+  })
+  test('Of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(listHelper.premadeBlogs)
+    assert.strictEqual(result, 36)
+  })
+})
