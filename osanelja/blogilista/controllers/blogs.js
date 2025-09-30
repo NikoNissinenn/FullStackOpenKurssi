462c2e034701049e6e264b328d59c3318eff4c17
Niko Nissinen <niko.nissinen@edu.riveria.fi>
2025-09-30 10:07:40 +0300

    Tehtävä 4.19

--- osanelja/blogilista/controllers/blogs.js
+++ osanelja/blogilista/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username:1, name:1, id:1 })
@@ -9,15 +18,16 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
 
-  const user = await User.find({})
+  const user = await User.findById(decodedToken.id)
 
   if (!user) {
     return response.status(400).json({ error: 'UserId missing or not valid' })
   }
-
-  const chosenUserIndex = Math.floor(Math.random() * user.length)
-  const chosenUser = user[chosenUserIndex]
 
   if (!body.likes) {
     body.likes = 0
@@ -40,12 +50,12 @@ blogsRouter.post('/', async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes,
-    user: chosenUser.id
+    user: user._id
   })
 
   const savedBlog = await blog.save()
-  chosenUser.blogs = chosenUser.blogs.concat(savedBlog._id)
-  await chosenUser.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
