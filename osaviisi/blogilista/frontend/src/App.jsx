d96132acad07d1a0fd420ceaa677854341e206d0
Niko Nissinen <niko.nissinen@edu.riveria.fi>
2025-10-01 11:53:26 +0300

    Tehtävä 5.3

--- osaviisi/blogilista/frontend/src/App.jsx
+++ osaviisi/blogilista/frontend/src/App.jsx
@@ -5,18 +5,21 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  // const [newBlog, setNewBlog] = useState('')
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [newBlog, setNewBlog] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll()
       .then(blogs =>
         setBlogs( blogs )
     )  
-  }, [])
+  }, [newBlog])
 
    useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
@@ -30,7 +33,7 @@ const App = () => {
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
-      const user = await loginService.login({ username, password }) 
+      const user = await loginService.login({ username, password })
       
       window.localStorage.setItem(
         'loggedBlogAppUser', JSON.stringify(user)
@@ -59,6 +62,29 @@ const App = () => {
     } catch (error) {
       console.log(error, error.message)
       setErrorMessage('Something went wrong when login out')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleNewBlog = async (event) => {
+    event.preventDefault()
+    try {
+      let createdBlog = {
+        title: title,
+        author: author,
+        url: url,
+        likes: 0
+      }
+      setNewBlog(createdBlog)
+      await blogService.create(createdBlog)
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+    } catch (error) {
+      console.log(error, error.message)
+      setErrorMessage('Something went wrong when creating new blog')
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000)
@@ -100,9 +126,53 @@ const App = () => {
         {`${user.username} logged in`}
         <button onClick={handleLogout}>Logout</button>
       </p>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+
+      <form onSubmit={handleNewBlog}>
+        <div>
+          <label>
+            Title:
+            <input
+              id='titlefield'
+              type="text"
+              value={title}
+              onChange={({ target }) => setTitle(target.value)}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>            
+            Author:
+            <input
+              id='authorfield'
+              type="text"
+              value={author}
+              onChange={({ target }) => setAuthor(target.value)}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Url:
+            <input
+              id='urlfield'
+              type="text"
+              value={url}
+              onChange={({ target }) => setUrl(target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">Create</button>
+      </form>
+
+      <div>
+        <ul>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </ul>
+      </div>
     </div>
   )
 
