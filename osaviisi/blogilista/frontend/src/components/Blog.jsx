492cc3d1be586c5f2328a677dc9fb8bb7c86ce2d
Niko Nissinen <niko.nissinen@edu.riveria.fi>
2025-10-01 14:18:44 +0300

    Tehtävä 5.7

--- osaviisi/blogilista/frontend/src/components/Blog.jsx
+++ osaviisi/blogilista/frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <li>
-    {blog.title} - {blog.author}
-  </li>  
-)
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [blogInfoVisible, setBlogInfoVisible] = useState(false)
+
+  const hideWhenVisible = { display: blogInfoVisible ? 'none' : '' }
+  const showWhenVisible = { display: blogInfoVisible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setBlogInfoVisible(!blogInfoVisible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+  <div style={blogStyle}>
+    <ul style={hideWhenVisible}>
+      <li>{blog.title} - {blog.author} <button onClick={toggleVisibility}>View</button></li>
+    </ul>
+
+    <ul style={showWhenVisible}>
+      <li>Title: {blog.title} <button onClick={toggleVisibility}>Hide</button></li>
+      <li>Url: {blog.url}</li>
+      <li>Likes: {blog.likes}</li>
+      <li>Author: {blog.author}</li>
+    </ul>
+  </div> 
+)}
 
 export default Blog
